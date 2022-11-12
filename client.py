9b5a61086b1548c3be99fbf65e7a73ee07be6fe9
NotAGameDev07 <lokeshspatel07@gmail.com>
2022-11-11 20:55:43 -0500

    Changed client.py and server.py

--- client.py
+++ client.py
@@ -1,14 +1,33 @@
 import socket
+import pickle
+
+HSIZE = 10
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.connect((socket.gethostname(), 1234))
 
-full_msg = ''
 while True:
-    msg = s.recv(10)
-    if len(msg) <= 0:
-        break
-    full_msg += msg.decode('utf-8')
+    full_msg = b''
+    new_msg = True
+    while True:
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length: {msg[:HSIZE]}")
+            try:
+                msglen = int(msg[:HSIZE])
+            except:
+                msglen = 0
+            new_msg = False
+        full_msg += msg
+        if len(full_msg) - HSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HSIZE:])
 
-print(full_msg)
+            d = pickle.loads(full_msg[HSIZE:])
+            print(d)
+
+            new_msg = True
+            full_msg = b''
+
+    print(full_msg)
